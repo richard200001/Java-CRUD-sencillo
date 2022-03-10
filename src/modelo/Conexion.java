f4c26ea541966d92d8997893ecd928e4e9d5a7d5
richard200001 <91160745+richard200001@users.noreply.github.com>
2022-03-09 23:24:30 -0500

    Update Conexion.java

--- src/modelo/Conexion.java
+++ src/modelo/Conexion.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package modelo;
 
 import java.sql.Connection;
@@ -10,10 +6,7 @@ import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.*;
-/**
- *
- * @author Usuario
- */
+
 public class Conexion {
     
     public static Connection obtenerConexion(){
